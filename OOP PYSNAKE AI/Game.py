f0abd6d44eacd6ed90562eb42826ae1783ba2534
David Berardi <goki7b@gmail.com>
2022-05-17 00:22:14 +0200

    Add files via upload

---
+++ OOP PYSNAKE AI/Game.py
@@ -0,0 +1,66 @@
+import pygame as pg
+from Snake import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.display.set_caption("SnakeAI")
+        self.WIN_SIZE = 500
+        self.TILE_SIZE = 50
+        self.screen = pg.display.set_mode([self.WIN_SIZE] * 2)
+        self.clock = pg.time.Clock()
+        self.FPS = 2500
+        self.grid_color = [50, 50, 50] #rgb
+        self.game_over = False
+        self.score = 0
+        self.font = pg.font.SysFont(None, 48)
+        self.reset()
+
+    def draw_grid(self):
+        # draws grid, line by line, for each axis
+        [pg.draw.line(self.screen, self.grid_color, (x, 0), (x, self.WIN_SIZE)) for x in range(0, self.WIN_SIZE, self.TILE_SIZE)]
+        [pg.draw.line(self.screen, self.grid_color, (0, y), (self.WIN_SIZE, y)) for y in range(0, self.WIN_SIZE, self.TILE_SIZE)]        
+    
+    def reset(self):
+        self.score = 0
+        self.iteration = 0
+        self.score_text = self.font.render(f"SCORE: {self.score}", True, (255, 255, 255))
+        self.snake = SnakeAI(self)
+        self.food = Food(self)
+        self.snake.direction = self.snake.right
+
+    def update(self, action):
+        self.iteration += 1
+
+        # user input
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+
+        # move
+        self.snake.controlAI(action)
+
+        # check game over
+        self.reward = 0
+        self.game_over = False
+        self.snake.check_borders() 
+        self.snake.check_cannibalism()
+
+        # check collision with food
+        self.snake.check_food()
+
+        # update screen and clock
+        self.draw()
+        self.clock.tick(self.FPS)
+
+        return self.reward, self.game_over, self.score       
+
+
+    def draw(self):
+        self.screen.fill("black")
+        self.draw_grid()
+        self.snake.draw()
+        self.food.draw()
+        self.screen.blit(self.score_text, (30, 50))
+        pg.display.flip()
---
+++ OOP PYSNAKE AI/Snake.py
@@ -0,0 +1,116 @@
+from random import randint, randrange
+import pygame as pg
+import numpy as np
+
+vec2 = pg.math.Vector2
+
+class SnakeAI:
+    def __init__(self, game):
+        self.game = game
+        self.size = game.TILE_SIZE
+        self.rect = pg.rect.Rect([0, 0, game.TILE_SIZE - 2, game.TILE_SIZE - 2])
+        self.rect.center = self.get_random_position()
+        self.direction = randint(1,4)
+        self.movement = vec2(0, 0)
+        self.step_delay = 0.1 # milliseconds
+        self.time = 0
+        self.length = 1
+        self.body = []
+        self.body.append(self.rect.copy())
+        self.body = self.body[-self.length:]
+        self.UP, self.DOWN = vec2(0, -self.size), vec2(0, self.size)
+        self.RIGHT, self.LEFT = vec2(-self.size, 0), vec2(self.size, 0)
+        self.right, self.left = 1, 2
+        self.up, self.down = 3, 4
+        #self.directions = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
+
+    def is_collision(self, pt):
+        # hits boundary
+        if pt[0] > (self.game.WIN_SIZE - self.game.TILE_SIZE - 2) or pt[0] < 0 or pt[1] > (self.game.WIN_SIZE - self.game.TILE_SIZE - 2) or pt[1] < 0:
+            return True
+        return False   
+
+    def delta_time(self):
+        time_now = pg.time.get_ticks()
+        if time_now - self.time > self.step_delay:
+            self.time = time_now
+            return True
+        return False 
+
+    def controlAI(self, action):
+        # AI input movement logic with just numbers, then converted in vec2() visual position
+        clockwise = [self.right, self.down, self.left, self.up]  # directions RIGHT, DOWN, LEFT, UP
+        current_direction = clockwise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_direction = clockwise[current_direction] # no change in direction
+        elif np.array_equal(action, [0, 1, 0]):
+            next_current_direction = (current_direction + 1) % 4
+            new_direction = clockwise[next_current_direction] # right turn r -> d -> l -> u
+        else: # [0, 0, 1]
+            next_current_direction = (current_direction - 1) % 4
+            new_direction = clockwise[next_current_direction] # left turn r -> u -> l -> d
+        
+        self.direction = new_direction
+
+        if self.direction == self.right:
+            self.movement = self.RIGHT
+    
+        if self.direction == self.left:
+            self.movement = self.LEFT
+        
+        if self.direction == self.up:
+            self.movement = self.UP
+        
+        if self.direction == self.down:
+            self.movement = self.DOWN
+
+        self.move()
+    
+    def move(self):
+        if self.delta_time():
+            self.rect.move_ip(self.movement)
+            self.body.append(self.rect.copy())
+            self.body = self.body[-self.length:]
+
+    def get_random_position(self):
+        return [randrange(self.size // 2, self.game.WIN_SIZE - self.size // 2, self.size)] * 2
+
+    def check_borders(self):
+        if self.rect.left < 0 or self.rect.right > self.game.WIN_SIZE or self.game.iteration > 100 * len(self.body):
+            self.game.reward -= 10
+            self.game.game_over = True
+            return self.game.reward, self.game.game_over, self.game.score
+
+        if self.rect.top < 0 or self.rect.bottom > self.game.WIN_SIZE or self.game.iteration > 100 * len(self.body):
+            self.game.reward -= 10
+            self.game.game_over = True
+            return self.game.reward, self.game.game_over, self.game.score
+
+    def check_food(self):
+        # if snake and food intersect, spawn new food randomly
+        if self.rect.center == self.game.food.rect.center:
+            self.game.food.rect.center = self.get_random_position()
+            self.length += 1
+            self.game.score += 1
+            self.game.score_text = self.game.font.render(f"SCORE: {self.game.score}", True, (255, 255, 255))
+            self.game.reward += 10
+
+    def check_cannibalism(self):
+        if len(self.body) != len(set(segment.center for segment in self.body)) or self.game.iteration > 100 * len(self.body):
+            self.game.reward -= 10
+            self.game.game_over = True
+            return self.game.reward, self.game.game_over, self.game.score
+
+    def draw(self):
+        [pg.draw.rect(self.game.screen, "green", segment) for segment in self.body]
+
+class Food:
+    def __init__(self, game):
+        self.game = game
+        self.size = game.TILE_SIZE
+        self.rect = pg.rect.Rect([0, 0, game.TILE_SIZE - 2, game.TILE_SIZE - 2])
+        self.rect.center = self.game.snake.get_random_position()
+
+    def draw(self):
+        pg.draw.rect(self.game.screen, "red", self.rect)
